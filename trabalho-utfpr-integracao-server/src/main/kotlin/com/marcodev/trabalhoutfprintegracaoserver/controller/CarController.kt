1f1ca7afd3e5ebd82f5defc410e336d6d92dd2d8
marco-rozo <marcorozo99@gmail.com>
2024-06-30 23:16:19 -0300

    add filter by name

--- trabalho-utfpr-integracao-server/src/main/kotlin/com/marcodev/trabalhoutfprintegracaoserver/controller/CarController.kt
+++ trabalho-utfpr-integracao-server/src/main/kotlin/com/marcodev/trabalhoutfprintegracaoserver/controller/CarController.kt
@@ -4,7 +4,14 @@ import com.marcodev.trabalhoutfprintegracaoserver.controller.dto.CarDTO
 import com.marcodev.trabalhoutfprintegracaoserver.repository.CarRepository
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import kotlin.jvm.optionals.getOrNull
 
 @RestController
@@ -13,10 +20,11 @@ class ClienteController(
     private val carRepository: CarRepository
 ) {
     @GetMapping
-    fun list(): List<CarDTO> {
-        return carRepository.findAll().map {
-            CarDTO.fromEntity(it)
-        }
+    fun list(@RequestParam name: String?): List<CarDTO> {
+        val cars =  name?.let {
+            carRepository.findByNameContainsIgnoreCase(it)
+        } ?: carRepository.findAll()
+        return cars.map { CarDTO.fromEntity(it) }
     }
 
     @PostMapping
--- trabalho-utfpr-integracao-server/src/main/kotlin/com/marcodev/trabalhoutfprintegracaoserver/repository/CarRepository.kt
+++ trabalho-utfpr-integracao-server/src/main/kotlin/com/marcodev/trabalhoutfprintegracaoserver/repository/CarRepository.kt
@@ -2,5 +2,9 @@ package com.marcodev.trabalhoutfprintegracaoserver.repository
 
 import com.marcodev.trabalhoutfprintegracaoserver.entity.Car
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-interface CarRepository : JpaRepository<Car, Int>
+interface CarRepository : JpaRepository<Car, Int> {
+    fun findByNameContainsIgnoreCase(name: String): List<Car>
+}
